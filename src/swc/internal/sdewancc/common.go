05366e46dcc02ea2f52f4b6ddd63e3bb2a35faf2
Yao, Le <le.yao@intel.com>
2022-06-23 12:45:26 +0800

    Add the sdewan cnf label to CR
    Signed-off-by: Yao, Le <le.yao@intel.com>

--- src/swc/internal/sdewancc/common.go
+++ src/swc/internal/sdewancc/common.go
@@ -6,15 +6,19 @@ package sdewancc
 const NETWORKING_SDEWAN_APIVERSION = "batch.sdewan.akraino.org/v1alpha1"
 
 type Metadata struct {
-	Name        string `yaml:"name"`
-	Namespace   string `yaml:"namespace,omitempty"`
-	Description string `yaml:"description,omitempty"`
+	Name        string            `yaml:"name"`
+	Namespace   string            `yaml:"namespace,omitempty"`
+	Labels      map[string]string `yaml:"labels,omitempty"`
+	Description string            `yaml:"description,omitempty"`
 }
 
 func createGenericMetadata(name string, namespace string, description string)(Metadata) {
+	labelmap := make(map[string]string)
+	labelmap["sdewanPurpose"] = "base"
 	var meta = Metadata {
 		Name:        name,
 		Namespace:   namespace,
+		Labels:      labelmap,
 		Description: description,
 	}
 	return meta
