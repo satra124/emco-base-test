jacobtrvl/emco

Adding logical cloud for GitOps based clusters
Includes changes to dcm to create service account
resource instead of a CSR for a namespace. Rsync
changes for fluxv2 plugin to support logical
clouds. Includes some formatting fixes for rsync

Signed-off-by: Ritu Sood <ritu.sood@intel.com>

src/dcm/pkg/module/apply.go
@@ -68,9 +68,10 @@ type RoleRules struct {
 }
 
 type RoleSubjects struct {
-	Kind     string `yaml:"kind"`
-	Name     string `yaml:"name"`
-	ApiGroup string `yaml:"apiGroup"`
+	Kind      string `yaml:"kind"`
+	Name      string `yaml:"name"`
+	ApiGroup  string `yaml:"apiGroup"`
+	NameSpace string `yaml:"namespace"`
 }
 
 type RoleRef struct {
@@ -117,6 +118,29 @@ func createNamespace(logicalcloud LogicalCloud) (string, string, error) {
 	}
 
 	return string(nsData), strings.Join([]string{name, "+Namespace"}, ""), nil
+}
+
+func createServiceAccount(logicalcloud LogicalCloud) (string, string, error) {
+
+	name := logicalcloud.Specification.NameSpace
+	labels := logicalcloud.Specification.Labels
+
+	sa := Resource{
+		ApiVersion: "v1",
+		Kind:       "ServiceAccount",
+		MetaData: MetaDatas{
+			Name:      strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "-sa"}, ""),
+			Labels:    labels,
+			Namespace: logicalcloud.Specification.NameSpace,
+		},
+	}
+
+	nsData, err := yaml.Marshal(&sa)
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(nsData), strings.Join([]string{name, "+ServiceAccount"}, ""), nil
 }
 
 func createRoles(logicalcloud LogicalCloud, userpermissions []UserPermission) ([]string, []string, error) {
@@ -168,7 +192,7 @@ func createRoles(logicalcloud LogicalCloud, userpermissions []UserPermission) ([
 	return datas, names, nil
 }
 
-func createRoleBindings(logicalcloud LogicalCloud, userpermissions []UserPermission) ([]string, []string, error) {
+func createRoleBindings(logicalcloud LogicalCloud, userpermissions []UserPermission, gitOpsSupport bool) ([]string, []string, error) {
 	var name string
 	var kind string
 	var kindbinding string
@@ -189,19 +213,29 @@ func createRoleBindings(logicalcloud LogicalCloud, userpermissions []UserPermiss
 			kind = "Role"
 			kindbinding = "RoleBinding"
 		}
+		var subjects []RoleSubjects
 
+		subjects = []RoleSubjects{RoleSubjects{
+			Kind:     "User",
+			Name:     logicalcloud.Specification.User.UserName,
+			ApiGroup: "",
+		},
+		}
+
+		if gitOpsSupport {
+			subjects = append(subjects, RoleSubjects{
+				Kind:      "ServiceAccount",
+				Name:      strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "-sa"}, ""),
+				NameSpace: up.Specification.Namespace,
+			})
+		}
 		roleBinding := Resource{
 			ApiVersion: "rbac.authorization.k8s.io/v1",
 			Kind:       kindbinding,
 			MetaData: MetaDatas{
 				Name: name,
 			},
-			Subjects: []RoleSubjects{RoleSubjects{
-				Kind:     "User",
-				Name:     logicalcloud.Specification.User.UserName,
-				ApiGroup: "",
-			},
-			},
+			Subjects: subjects,
 
 			RoleRefs: RoleRef{
 				Kind:     kind,
@@ -432,6 +466,11 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud LogicalCloud, cluster C
 		return cleanupCompositeApp(context, err, "Error adding Cluster to AppContext", details)
 	}
 
+	gitOps, err := IsGitOpsCluster(clusterName)
+	if err != nil {
+		return pkgerrors.Wrap(err, "Error reading admin cloud config")
+	}
+
 	// Get resources to be added
 	namespace, namespaceName, err := createNamespace(logicalcloud)
 	if err != nil {
@@ -443,7 +482,7 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud LogicalCloud, cluster C
 		return pkgerrors.Wrap(err, "Error Creating Roles/ClusterRoles YAMLs for logical cloud")
 	}
 
-	roleBindings, roleBindingNames, err := createRoleBindings(logicalcloud, userPermissionList)
+	roleBindings, roleBindingNames, err := createRoleBindings(logicalcloud, userPermissionList, gitOps)
 	if err != nil {
 		return pkgerrors.Wrap(err, "Error Creating RoleBindings/ClusterRoleBindings YAMLs for logical cloud")
 	}
@@ -453,39 +492,63 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud LogicalCloud, cluster C
 		return pkgerrors.Wrap(err, "Error Creating Quota YAMLs for logical cloud")
 	}
 
-	// then use it to generate a CSR for the cluster being processed
-	csr, csrName, err := createUserCSR(logicalcloud, pkData)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating User CSR and Key for logical cloud")
-	}
-
-	// check to see if - in case of an update - if the csr was already created for this logical cloud
-	gotCsr, oldCsr := alreadyGotCsr(oldCid, context, lcAppName, clusterName, csrName)
-	if gotCsr {
-		csr = oldCsr
-	}
-
-	approval, err := createApprovalSubresource(logicalcloud)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating approval subresource for logical cloud")
-	}
-
 	// Add namespace resource to each cluster
 	_, err = context.AddResource(clusterHandle, namespaceName, namespace)
 	if err != nil {
 		return cleanupCompositeApp(context, err, "Error adding Namespace Resource to AppContext", details)
 	}
+	resorderList := []string{namespaceName}
+	if !gitOps {
+		// then use it to generate a CSR for the cluster being processed
+		csr, csrName, err := createUserCSR(logicalcloud, pkData)
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error Creating User CSR and Key for logical cloud")
+		}
 
-	// Add csr resource to each cluster
-	csrHandle, err := context.AddResource(clusterHandle, csrName, csr)
-	if err != nil {
-		return cleanupCompositeApp(context, err, "Error adding CSR Resource to AppContext", details)
-	}
+		// check to see if - in case of an update - if the csr was already created for this logical cloud
+		gotCsr, oldCsr := alreadyGotCsr(oldCid, context, lcAppName, clusterName, csrName)
+		if gotCsr {
+			csr = oldCsr
+		}
 
-	// Add csr approval as a subresource of csr
-	_, err = context.AddLevelValue(csrHandle, "subresource/approval", approval)
-	if err != nil {
-		return cleanupCompositeApp(context, err, "Error approving CSR via AppContext", details)
+		approval, err := createApprovalSubresource(logicalcloud)
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error Creating approval subresource for logical cloud")
+		}
+		// Add csr resource to each cluster
+		csrHandle, err := context.AddResource(clusterHandle, csrName, csr)
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error adding CSR Resource to AppContext", details)
+		}
+
+		// Add csr approval as a subresource of csr
+		_, err = context.AddLevelValue(csrHandle, "subresource/approval", approval)
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error approving CSR via AppContext", details)
+		}
+		resorderList = append(resorderList, csrName)
+
+		// Add Subresource Order and Subresource Dependency
+		subresOrder, err := json.Marshal(map[string][]string{"subresorder": []string{"approval"}})
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error creating subresource order JSON")
+		}
+
+		_, err = context.AddInstruction(csrHandle, "subresource", "order", string(subresOrder))
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error adding instruction order to AppContext", details)
+		}
+	} else {
+		sa, saName, err := createServiceAccount(logicalcloud)
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error Creating SA for logical cloud")
+		}
+		// Add sa resource to each cluster
+		_, err = context.AddResource(clusterHandle, saName, sa)
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error adding SA Resource to AppContext", details)
+		}
+		resorderList = append(resorderList, saName)
 	}
 
 	// Add [Cluster]Role resources to each cluster
@@ -512,14 +575,7 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud LogicalCloud, cluster C
 		}
 	}
 
-	// Add Subresource Order and Subresource Dependency
-	subresOrder, err := json.Marshal(map[string][]string{"subresorder": []string{"approval"}})
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error creating subresource order JSON")
-	}
-
 	// Add Resource Order
-	resorderList := []string{namespaceName, csrName}
 	resorderList = append(resorderList, quotaNames...)
 	resorderList = append(resorderList, roleNames...)
 	resorderList = append(resorderList, roleBindingNames...)
@@ -530,10 +586,6 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud LogicalCloud, cluster C
 
 	// Add Resource-level Order
 	_, err = context.AddInstruction(clusterHandle, "resource", "order", string(resOrder))
-	if err != nil {
-		return cleanupCompositeApp(context, err, "Error adding instruction order to AppContext", details)
-	}
-	_, err = context.AddInstruction(csrHandle, "subresource", "order", string(subresOrder))
 	if err != nil {
 		return cleanupCompositeApp(context, err, "Error adding instruction order to AppContext", details)
 	}
@@ -735,10 +787,11 @@ func blindInstantiateL1(oldCid string, project string, logicalcloud LogicalCloud
 	// project name, logical cloud name, level="0" and namespace="default"
 	err = context.AddCompositeAppMeta(
 		appcontext.CompositeAppMeta{
-			Project:      project,
-			LogicalCloud: logicalCloudName,
-			Level:        "0",
-			Namespace:    "default"})
+			Project:               project,
+			LogicalCloud:          logicalCloudName,
+			LogicalCloudNamespace: logicalcloud.Specification.NameSpace,
+			Level:                 "0",
+			Namespace:             "default"})
 	if err != nil {
 		return context, "", cleanupCompositeApp(context, err, "Error Adding Logical Cloud Meta to AppContext", []string{logicalCloudName, cid})
 	}
@@ -1039,4 +1092,23 @@ func Stop(project string, logicalcloud LogicalCloud) error {
 	}
 
 	return nil
+}
+
+func IsGitOpsCluster(clustername string) (bool, error) {
+
+	if !strings.Contains(clustername, "+") {
+		return false, pkgerrors.New("Not a valid cluster name")
+	}
+	strs := strings.Split(clustername, "+")
+	if len(strs) != 2 {
+		return false, pkgerrors.New("Not a valid cluster name")
+	}
+
+	ccc := rsync.NewCloudConfigClient()
+
+	_, err := ccc.GetGitOpsConfig(strs[0], strs[1], "0", "")
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
 }

src/rsync/pkg/plugins/fluxv2/config.go
@@ -13,37 +13,66 @@ import (
 	yaml "github.com/ghodss/yaml"
 	log "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
 	emcogit "gitlab.com/project-emco/core/emco-base/src/rsync/pkg/gitops/emcogit"
+	"gitlab.com/project-emco/core/emco-base/src/rsync/pkg/internal/utils"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Create GitRepository and Kustomization CR's for Flux
 func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) error {
 
-	// Create Source CR and Kcustomize CR
-	gr := fluxsc.GitRepository{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "source.toolkit.fluxcd.io/v1beta1",
-			Kind:       "GitRepository",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      p.gitProvider.Cid,
-			Namespace: p.gitProvider.Namespace,
-		},
-		Spec: fluxsc.GitRepositorySpec{
-			URL:       p.gitProvider.Url,
-			Interval:  metav1.Duration{Duration: time.Second * 30},
-			Reference: &fluxsc.GitRepositoryRef{Branch: p.gitProvider.Branch},
-		},
-	}
-	x, err := yaml.Marshal(&gr)
+	var sa string
+	acUtils, err := utils.NewAppContextReference(p.gitProvider.Cid)
 	if err != nil {
-		log.Error("ApplyConfig:: Marshal err", log.Fields{"err": err, "gr": gr})
+		return nil
+	}
+	_, level := acUtils.GetNamespace()
+	_, lc, lcn, err := acUtils.GetLogicalCloudInfo()
+	if err != nil {
 		return err
 	}
-	path := "clusters/" + p.gitProvider.Cluster + "/" + gr.Name + ".yaml"
-	// Add to the commit
-	gp := emcogit.Add(path, string(x), []gitprovider.CommitFile{}, p.gitProvider.GitType)
+	if level == "1" {
+		sa = lc + "-sa"
+	}
+	var namespace, kName string
+	var skip bool
+	var gp interface{}
 
+	// Special case creating a logical cloud
+	if level == "0" && lcn != "" {
+		namespace = "flux-system"
+		kName = "flux-system"
+		skip = true
+	} else {
+		namespace = p.gitProvider.Namespace
+		skip = false
+	}
+	if !skip {
+		// Create Source CR and Kcustomize CR
+		gr := fluxsc.GitRepository{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "source.toolkit.fluxcd.io/v1beta1",
+				Kind:       "GitRepository",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      p.gitProvider.Cid,
+				Namespace: p.gitProvider.Namespace,
+			},
+			Spec: fluxsc.GitRepositorySpec{
+				URL:       p.gitProvider.Url,
+				Interval:  metav1.Duration{Duration: time.Second * 30},
+				Reference: &fluxsc.GitRepositoryRef{Branch: p.gitProvider.Branch},
+			},
+		}
+		x, err := yaml.Marshal(&gr)
+		if err != nil {
+			log.Error("ApplyConfig:: Marshal err", log.Fields{"err": err, "gr": gr})
+			return err
+		}
+		path := "clusters/" + p.gitProvider.Cluster + "/" + gr.Name + ".yaml"
+		// Add to the commit
+		gp = emcogit.Add(path, string(x), []gitprovider.CommitFile{}, p.gitProvider.GitType)
+		kName = gr.Name
+	}
 	kc := kustomize.Kustomization{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "kustomize.toolkit.fluxcd.io/v1beta2",
@@ -51,7 +80,7 @@ func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) er
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "kcust" + p.gitProvider.Cid,
-			Namespace: p.gitProvider.Namespace,
+			Namespace: namespace,
 		},
 		Spec: kustomize.KustomizationSpec{
 			Interval: metav1.Duration{Duration: time.Second * 300},
@@ -59,9 +88,9 @@ func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) er
 			Prune:    true,
 			SourceRef: kustomize.CrossNamespaceSourceReference{
 				Kind: "GitRepository",
-				Name: gr.Name,
+				Name: kName,
 			},
-			TargetNamespace: p.gitProvider.Namespace,
+			ServiceAccountName: sa,
 		},
 	}
 	y, err := yaml.Marshal(&kc)
@@ -69,7 +98,7 @@ func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) er
 		log.Error("ApplyConfig:: Marshal err", log.Fields{"err": err, "kc": kc})
 		return err
 	}
-	path = "clusters/" + p.gitProvider.Cluster + "/" + kc.Name + ".yaml"
+	path := "clusters/" + p.gitProvider.Cluster + "/" + kc.Name + ".yaml"
 	gp = emcogit.Add(path, string(y), gp, p.gitProvider.GitType)
 	// Commit
 	err = emcogit.CommitFiles(ctx, p.gitProvider.Client, p.gitProvider.UserName, p.gitProvider.RepoName, p.gitProvider.Branch, "Commit for "+p.gitProvider.GetPath("context"), gp, p.gitProvider.GitType)

satra124/emco-base-test

src/dcm/pkg/module/apply.go
@@ -69,9 +69,10 @@ type RoleRules struct {
 }
 
 type RoleSubjects struct {
-	Kind     string `yaml:"kind"`
-	Name     string `yaml:"name"`
-	ApiGroup string `yaml:"apiGroup"`
+	Kind      string `yaml:"kind"`
+	Name      string `yaml:"name"`
+	ApiGroup  string `yaml:"apiGroup"`
+	NameSpace string `yaml:"namespace"`
 }
 
 type RoleRef struct {
@@ -118,6 +119,29 @@ func createNamespace(logicalcloud common.LogicalCloud) (string, string, error) {
 	}
 
 	return string(nsData), strings.Join([]string{name, "+Namespace"}, ""), nil
+}
+
+func createServiceAccount(logicalcloud common.LogicalCloud) (string, string, error) {
+
+	name := logicalcloud.Specification.NameSpace
+	labels := logicalcloud.Specification.Labels
+
+	sa := Resource{
+		ApiVersion: "v1",
+		Kind:       "ServiceAccount",
+		MetaData: MetaDatas{
+			Name:      strings.Join([]string{name, "-sa"}, ""),
+			Labels:    labels,
+			Namespace: name,
+		},
+	}
+
+	nsData, err := yaml.Marshal(&sa)
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(nsData), strings.Join([]string{name, "+ServiceAccount"}, ""), nil
 }
 
 func createRoles(logicalcloud common.LogicalCloud, userpermissions []UserPermission) ([]string, []string, error) {
@@ -169,7 +193,7 @@ func createRoles(logicalcloud common.LogicalCloud, userpermissions []UserPermiss
 	return datas, names, nil
 }
 
-func createRoleBindings(logicalcloud common.LogicalCloud, userpermissions []UserPermission) ([]string, []string, error) {
+func createRoleBindings(logicalcloud common.LogicalCloud, userpermissions []UserPermission, gitOpsSupport bool) ([]string, []string, error) {
 	var name string
 	var kind string
 	var kindbinding string
@@ -190,19 +214,29 @@ func createRoleBindings(logicalcloud common.LogicalCloud, userpermissions []User
 			kind = "Role"
 			kindbinding = "RoleBinding"
 		}
+		var subjects []RoleSubjects
 
+		subjects = []RoleSubjects{RoleSubjects{
+			Kind:     "User",
+			Name:     logicalcloud.Specification.User.UserName,
+			ApiGroup: "",
+		},
+		}
+
+		if gitOpsSupport {
+			subjects = append(subjects, RoleSubjects{
+				Kind:      "ServiceAccount",
+				Name:      strings.Join([]string{logicalcloud.MetaData.Name, "-sa"}, ""),
+				NameSpace: up.Specification.Namespace,
+			})
+		}
 		roleBinding := Resource{
 			ApiVersion: "rbac.authorization.k8s.io/v1",
 			Kind:       kindbinding,
 			MetaData: MetaDatas{
 				Name: name,
 			},
-			Subjects: []RoleSubjects{RoleSubjects{
-				Kind:     "User",
-				Name:     logicalcloud.Specification.User.UserName,
-				ApiGroup: "",
-			},
-			},
+			Subjects: subjects,
 
 			RoleRefs: RoleRef{
 				Kind:     kind,
@@ -433,6 +467,11 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud common.LogicalCloud, cl
 		return cleanupCompositeApp(context, err, "Error adding Cluster to AppContext", details)
 	}
 
+	gitOps, err := IsGitOpsCluster(clusterName)
+	if err != nil {
+		return err
+	}
+
 	// Get resources to be added
 	namespace, namespaceName, err := createNamespace(logicalcloud)
 	if err != nil {
@@ -444,7 +483,7 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud common.LogicalCloud, cl
 		return pkgerrors.Wrap(err, "Error Creating Roles/ClusterRoles YAMLs for logical cloud")
 	}
 
-	roleBindings, roleBindingNames, err := createRoleBindings(logicalcloud, userPermissionList)
+	roleBindings, roleBindingNames, err := createRoleBindings(logicalcloud, userPermissionList, gitOps)
 	if err != nil {
 		return pkgerrors.Wrap(err, "Error Creating RoleBindings/ClusterRoleBindings YAMLs for logical cloud")
 	}
@@ -454,39 +493,63 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud common.LogicalCloud, cl
 		return pkgerrors.Wrap(err, "Error Creating Quota YAMLs for logical cloud")
 	}
 
-	// then use it to generate a CSR for the cluster being processed
-	csr, csrName, err := createUserCSR(logicalcloud, pkData)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating User CSR and Key for logical cloud")
-	}
-
-	// check to see if - in case of an update - if the csr was already created for this logical cloud
-	gotCsr, oldCsr := alreadyGotCsr(oldCid, context, lcAppName, clusterName, csrName)
-	if gotCsr {
-		csr = oldCsr
-	}
-
-	approval, err := createApprovalSubresource(logicalcloud)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating approval subresource for logical cloud")
-	}
-
 	// Add namespace resource to each cluster
 	_, err = context.AddResource(clusterHandle, namespaceName, namespace)
 	if err != nil {
 		return cleanupCompositeApp(context, err, "Error adding Namespace Resource to AppContext", details)
 	}
+	resorderList := []string{namespaceName}
+	if !gitOps {
+		// then use it to generate a CSR for the cluster being processed
+		csr, csrName, err := createUserCSR(logicalcloud, pkData)
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error Creating User CSR and Key for logical cloud")
+		}
 
-	// Add csr resource to each cluster
-	csrHandle, err := context.AddResource(clusterHandle, csrName, csr)
-	if err != nil {
-		return cleanupCompositeApp(context, err, "Error adding CSR Resource to AppContext", details)
-	}
+		// check to see if - in case of an update - if the csr was already created for this logical cloud
+		gotCsr, oldCsr := alreadyGotCsr(oldCid, context, lcAppName, clusterName, csrName)
+		if gotCsr {
+			csr = oldCsr
+		}
 
-	// Add csr approval as a subresource of csr
-	_, err = context.AddLevelValue(csrHandle, "subresource/approval", approval)
-	if err != nil {
-		return cleanupCompositeApp(context, err, "Error approving CSR via AppContext", details)
+		approval, err := createApprovalSubresource(logicalcloud)
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error Creating approval subresource for logical cloud")
+		}
+		// Add csr resource to each cluster
+		csrHandle, err := context.AddResource(clusterHandle, csrName, csr)
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error adding CSR Resource to AppContext", details)
+		}
+
+		// Add csr approval as a subresource of csr
+		_, err = context.AddLevelValue(csrHandle, "subresource/approval", approval)
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error approving CSR via AppContext", details)
+		}
+		resorderList = append(resorderList, csrName)
+
+		// Add Subresource Order and Subresource Dependency
+		subresOrder, err := json.Marshal(map[string][]string{"subresorder": []string{"approval"}})
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error creating subresource order JSON")
+		}
+
+		_, err = context.AddInstruction(csrHandle, "subresource", "order", string(subresOrder))
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error adding instruction order to AppContext", details)
+		}
+	} else {
+		sa, saName, err := createServiceAccount(logicalcloud)
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error Creating SA for logical cloud")
+		}
+		// Add sa resource to each cluster
+		_, err = context.AddResource(clusterHandle, saName, sa)
+		if err != nil {
+			return cleanupCompositeApp(context, err, "Error adding SA Resource to AppContext", details)
+		}
+		resorderList = append(resorderList, saName)
 	}
 
 	// Add [Cluster]Role resources to each cluster
@@ -513,14 +576,7 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud common.LogicalCloud, cl
 		}
 	}
 
-	// Add Subresource Order and Subresource Dependency
-	subresOrder, err := json.Marshal(map[string][]string{"subresorder": []string{"approval"}})
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error creating subresource order JSON")
-	}
-
 	// Add Resource Order
-	resorderList := []string{namespaceName, csrName}
 	resorderList = append(resorderList, quotaNames...)
 	resorderList = append(resorderList, roleNames...)
 	resorderList = append(resorderList, roleBindingNames...)
@@ -531,10 +587,6 @@ func prepL1ClusterAppContext(oldCid string, logicalcloud common.LogicalCloud, cl
 
 	// Add Resource-level Order
 	_, err = context.AddInstruction(clusterHandle, "resource", "order", string(resOrder))
-	if err != nil {
-		return cleanupCompositeApp(context, err, "Error adding instruction order to AppContext", details)
-	}
-	_, err = context.AddInstruction(csrHandle, "subresource", "order", string(subresOrder))
 	if err != nil {
 		return cleanupCompositeApp(context, err, "Error adding instruction order to AppContext", details)
 	}
@@ -736,10 +788,11 @@ func blindInstantiateL1(oldCid string, project string, logicalcloud common.Logic
 	// project name, logical cloud name, level="0" and namespace="default"
 	err = context.AddCompositeAppMeta(
 		appcontext.CompositeAppMeta{
-			Project:      project,
-			LogicalCloud: logicalCloudName,
-			Level:        "0",
-			Namespace:    "default"})
+			Project:               project,
+			LogicalCloud:          logicalCloudName,
+			LogicalCloudNamespace: logicalcloud.Specification.NameSpace,
+			Level:                 "0",
+			Namespace:             "default"})
 	if err != nil {
 		return context, "", cleanupCompositeApp(context, err, "Error Adding Logical Cloud Meta to AppContext", []string{logicalCloudName, cid})
 	}
@@ -1037,4 +1090,31 @@ func Stop(project string, logicalcloud common.LogicalCloud) error {
 	}
 
 	return nil
+}
+
+func IsGitOpsCluster(clustername string) (bool, error) {
+
+	if !strings.Contains(clustername, "+") {
+		log.Debug("GitOps Not a valid cluster name", log.Fields{})
+		return false, pkgerrors.New("GitOps Not a valid cluster name")
+	}
+	strs := strings.Split(clustername, "+")
+	if len(strs) != 2 {
+		log.Debug("GitOps Not a valid cluster name", log.Fields{})
+		return false, pkgerrors.New("GitOps Not a valid cluster name")
+	}
+
+	ccc := rsync.NewCloudConfigClient()
+
+	gc, err := ccc.GetGitOpsConfig(strs[0], strs[1], "0", "")
+	if err != nil {
+		log.Debug("Error getting GitOps config", log.Fields{"err": err})
+		return false, nil
+	}
+	if gc.Config.Props.GitOpsType == "fluxcd" || gc.Config.Props.GitOpsType == "azureArc" {
+		return true, nil
+	} else {
+		log.Info("GitOps Type not supported:", log.Fields{"GitOpsType": gc.Config.Props.GitOpsType})
+		return false, pkgerrors.New("GitOps Type not supported: " + gc.Config.Props.GitOpsType)
+	}
 }

src/rsync/pkg/plugins/fluxv2/config.go
@@ -13,45 +13,74 @@ import (
 	yaml "github.com/ghodss/yaml"
 	log "gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
 	emcogit "gitlab.com/project-emco/core/emco-base/src/rsync/pkg/gitops/emcogit"
+	"gitlab.com/project-emco/core/emco-base/src/rsync/pkg/internal/utils"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Create GitRepository and Kustomization CR's for Flux
 func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) error {
 
-	// Create Source CR and Kcustomize CR
-	gr := fluxsc.GitRepository{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "source.toolkit.fluxcd.io/v1beta1",
-			Kind:       "GitRepository",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      p.gitProvider.Cid,
-			Namespace: p.gitProvider.Namespace,
-		},
-		Spec: fluxsc.GitRepositorySpec{
-			URL:       p.gitProvider.Url,
-			Interval:  metav1.Duration{Duration: time.Second * 30},
-			Reference: &fluxsc.GitRepositoryRef{Branch: p.gitProvider.Branch},
-		},
-	}
-	x, err := yaml.Marshal(&gr)
+	var sa string
+	acUtils, err := utils.NewAppContextReference(p.gitProvider.Cid)
 	if err != nil {
-		log.Error("ApplyConfig:: Marshal err", log.Fields{"err": err, "gr": gr})
+		return nil
+	}
+	_, level := acUtils.GetNamespace()
+	_, _, lcn, err := acUtils.GetLogicalCloudInfo()
+	if err != nil {
 		return err
 	}
-	path := "clusters/" + p.gitProvider.Cluster + "/" + gr.Name + ".yaml"
-	// Add to the commit
-	gp := emcogit.Add(path, string(x), []gitprovider.CommitFile{}, p.gitProvider.GitType)
+	if level == "1" {
+		sa = lcn + "-sa"
+	}
+	var namespace, kName string
+	var skip bool
+	var gp interface{}
 
+	// Special case creating a logical cloud
+	if level == "0" && lcn != "" {
+		namespace = "flux-system"
+		kName = "flux-system"
+		skip = true
+	} else {
+		namespace = p.gitProvider.Namespace
+		skip = false
+	}
+	if !skip {
+		// Create Source CR and KustomizeCR
+		gr := fluxsc.GitRepository{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "source.toolkit.fluxcd.io/v1beta1",
+				Kind:       "GitRepository",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      p.gitProvider.Cid,
+				Namespace: p.gitProvider.Namespace,
+			},
+			Spec: fluxsc.GitRepositorySpec{
+				URL:       p.gitProvider.Url,
+				Interval:  metav1.Duration{Duration: time.Second * 30},
+				Reference: &fluxsc.GitRepositoryRef{Branch: p.gitProvider.Branch},
+			},
+		}
+		x, err := yaml.Marshal(&gr)
+		if err != nil {
+			log.Error("ApplyConfig:: Marshal err", log.Fields{"err": err, "gr": gr})
+			return err
+		}
+		path := "clusters/" + p.gitProvider.Cluster + "/" + gr.Name + ".yaml"
+		// Add to the commit
+		gp = emcogit.Add(path, string(x), []gitprovider.CommitFile{}, p.gitProvider.GitType)
+		kName = gr.Name
+	}
 	kc := kustomize.Kustomization{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "kustomize.toolkit.fluxcd.io/v1beta2",
 			Kind:       "Kustomization",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "kcust" + p.gitProvider.Cid,
-			Namespace: p.gitProvider.Namespace,
+			Name:      "kust" + p.gitProvider.Cid,
+			Namespace: namespace,
 		},
 		Spec: kustomize.KustomizationSpec{
 			Interval: metav1.Duration{Duration: time.Second * 300},
@@ -59,9 +88,9 @@ func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) er
 			Prune:    true,
 			SourceRef: kustomize.CrossNamespaceSourceReference{
 				Kind: "GitRepository",
-				Name: gr.Name,
+				Name: kName,
 			},
-			TargetNamespace: p.gitProvider.Namespace,
+			ServiceAccountName: sa,
 		},
 	}
 	y, err := yaml.Marshal(&kc)
@@ -69,7 +98,7 @@ func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) er
 		log.Error("ApplyConfig:: Marshal err", log.Fields{"err": err, "kc": kc})
 		return err
 	}
-	path = "clusters/" + p.gitProvider.Cluster + "/" + kc.Name + ".yaml"
+	path := "clusters/" + p.gitProvider.Cluster + "/" + kc.Name + ".yaml"
 	gp = emcogit.Add(path, string(y), gp, p.gitProvider.GitType)
 	// Commit
 	appName := p.gitProvider.Cid + "-" + p.gitProvider.App + "-config"
@@ -84,7 +113,7 @@ func (p *Fluxv2Provider) ApplyConfig(ctx context.Context, config interface{}) er
 func (p *Fluxv2Provider) DeleteConfig(ctx context.Context, config interface{}) error {
 	path := "clusters/" + p.gitProvider.Cluster + "/" + p.gitProvider.Cid + ".yaml"
 	gp := emcogit.Delete(path, []gitprovider.CommitFile{}, p.gitProvider.GitType)
-	path = "clusters/" + p.gitProvider.Cluster + "/" + "kcust" + p.gitProvider.Cid + ".yaml"
+	path = "clusters/" + p.gitProvider.Cluster + "/" + "kust" + p.gitProvider.Cid + ".yaml"
 	gp = emcogit.Delete(path, gp, p.gitProvider.GitType)
 	appName := p.gitProvider.Cid + "-" + p.gitProvider.App + "-config"
 	err := emcogit.CommitFiles(ctx, p.gitProvider.Client, p.gitProvider.UserName, p.gitProvider.RepoName, p.gitProvider.Branch, "Commit for "+p.gitProvider.GetPath("context"), appName, gp, p.gitProvider.GitType)