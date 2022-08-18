jacobtrvl/emco

emco error handling
 - Remove the API error list
 - Create a new error package to handle the errors
 - Define emco-specific error types to handle the errors
 - Handle HTTP status based on the error type
 - Handle resource lifecycle event errors based on the event type

Signed-off-by: Subin John <subin.john@intel.com>

src/ca-certs/pkg/client/logicalcloud/logicalcloud.go
@@ -6,7 +6,8 @@ package logicalcloud
 import (
 	"reflect"
 
-	"github.com/pkg/errors"
+	"gitlab.com/project-emco/core/emco-base/src/ca-certs/pkg/module"
+	"gitlab.com/project-emco/core/emco-base/src/orchestrator/common/emcoerror"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/db"
 )
 
@@ -53,7 +54,10 @@ func (c *CaCertLogicalCloudClient) CreateLogicalCloud(logicalCloud CaCertLogical
 
 	if lcExists &&
 		failIfExists {
-		return CaCertLogicalCloud{}, lcExists, errors.New("LogicalCloud already exists")
+		return CaCertLogicalCloud{}, lcExists, &emcoerror.Error{
+			Message: module.CaCertLogicalCloudAlreadyExists,
+			Type:    emcoerror.Conflict,
+		}
 	}
 
 	if err := db.DBconn.Insert(c.dbInfo.StoreName, key, nil, c.dbInfo.TagMeta, logicalCloud); err != nil {
@@ -109,7 +113,10 @@ func (c *CaCertLogicalCloudClient) GetLogicalCloud(logicalCloud, cert, project s
 	}
 
 	if len(value) == 0 {
-		return CaCertLogicalCloud{}, errors.New("LogicalCloud not found")
+		return CaCertLogicalCloud{}, &emcoerror.Error{
+			Message: module.CaCertLogicalCloudNotFound,
+			Type:    emcoerror.NotFound,
+		}
 	}
 
 	if value != nil {
@@ -120,5 +127,8 @@ func (c *CaCertLogicalCloudClient) GetLogicalCloud(logicalCloud, cert, project s
 		return lc, nil
 	}
 
-	return CaCertLogicalCloud{}, errors.New("Unknown Error")
+	return CaCertLogicalCloud{}, &emcoerror.Error{
+		Message: emcoerror.UnknownErrorMessage,
+		Type:    emcoerror.Unknown,
+	}
 }

src/ca-certs/pkg/module/cert.go
@@ -4,9 +4,10 @@
 package module
 
 import (
+	"fmt"
 	"reflect"
 
-	"github.com/pkg/errors"
+	"gitlab.com/project-emco/core/emco-base/src/orchestrator/common/emcoerror"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/appcontext"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/db"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
@@ -48,7 +49,10 @@ func (c *CaCertClient) CreateCert(cert CaCert, failIfExists bool) (CaCert, bool,
 
 	if certExists &&
 		failIfExists {
-		return CaCert{}, certExists, errors.New("Certificate already exists")
+		return CaCert{}, certExists, &emcoerror.Error{
+			Message: CaCertAlreadyExists,
+			Type:    emcoerror.Conflict,
+		}
 	}
 
 	if err := db.DBconn.Insert(c.dbInfo.StoreName, c.dbKey, nil, c.dbInfo.TagMeta, cert); err != nil {
@@ -90,7 +94,10 @@ func (c *CaCertClient) GetCert() (CaCert, error) {
 	}
 
 	if len(value) == 0 {
-		return CaCert{}, errors.New("Certificate not found")
+		return CaCert{}, &emcoerror.Error{
+			Message: CaCertNotFound,
+			Type:    emcoerror.NotFound,
+		}
 	}
 
 	if value != nil {
@@ -101,7 +108,10 @@ func (c *CaCertClient) GetCert() (CaCert, error) {
 		return cert, nil
 	}
 
-	return CaCert{}, errors.New("Unknown Error")
+	return CaCert{}, &emcoerror.Error{
+		Message: emcoerror.UnknownErrorMessage,
+		Type:    emcoerror.Unknown,
+	}
 }
 
 // UpdateCert update the caCert
@@ -124,7 +134,10 @@ func (c *CaCertClient) VerifyStateBeforeDelete(cert, lifecycle string) error {
 
 	if cState == state.StateEnum.Instantiated ||
 		cState == state.StateEnum.InstantiateStopped {
-		err := errors.Errorf("%s must be terminated for CaCert %s before it can be deleted", lifecycle, cert)
+		err := &emcoerror.Error{
+			Message: fmt.Sprintf("%s must be terminated for CaCert %s before it can be deleted", lifecycle, cert),
+			Type:    emcoerror.Conflict,
+		}
 		logutils.Error("",
 			logutils.Fields{
 				"Error": err.Error()})
@@ -139,7 +152,10 @@ func (c *CaCertClient) VerifyStateBeforeDelete(cert, lifecycle string) error {
 		if err == nil &&
 			!(acStatus.Status == appcontext.AppContextStatusEnum.Terminated ||
 				acStatus.Status == appcontext.AppContextStatusEnum.TerminateFailed) {
-			err := errors.Errorf("%s termination has not completed for CaCert %s", lifecycle, cert)
+			err := &emcoerror.Error{
+				Message: fmt.Sprintf("%s termination has not completed for CaCert %s", lifecycle, cert),
+				Type:    emcoerror.Conflict,
+			}
 			logutils.Error("",
 				logutils.Fields{
 					"Error": err.Error()})
@@ -148,6 +164,7 @@ func (c *CaCertClient) VerifyStateBeforeDelete(cert, lifecycle string) error {
 
 		for _, id := range state.GetContextIdsFromStateInfo(stateInfo) {
 			context, err := state.GetAppContextFromId(id)
+			// TODO:- check how to handle this
 			if err != nil {
 				logutils.Error("Failed to get appContext from id",
 					logutils.Fields{
@@ -155,6 +172,7 @@ func (c *CaCertClient) VerifyStateBeforeDelete(cert, lifecycle string) error {
 						"Error": err.Error()})
 				return err
 			}
+			// TODO:- check how to handle this
 			err = context.DeleteCompositeApp()
 			if err != nil {
 				logutils.Error("Failed to delete the appContext",
@@ -182,7 +200,10 @@ func (c *CaCertClient) VerifyStateBeforeUpdate(cert, lifecycle string) error {
 	}
 
 	if cState != state.StateEnum.Created {
-		err := errors.Errorf("Failed to update the CaCert. %s for the CaCert %s is in %s state", lifecycle, cert, cState)
+		err := &emcoerror.Error{
+			Message: fmt.Sprintf("Failed to update the CaCert. %s for the CaCert %s is in %s state", lifecycle, cert, cState),
+			Type:    emcoerror.Conflict,
+		}
 		logutils.Error("",
 			logutils.Fields{
 				"Error": err.Error()})

satra124/emco-base-test

 - Define emco-specific error reasons to handle the errors
 - Handle HTTP status based on the error reason
 - Developer doc

src/ca-certs/pkg/client/logicalcloud/logicalcloud.go
@@ -6,7 +6,8 @@ package logicalcloud
 import (
 	"reflect"
 
-	"github.com/pkg/errors"
+	"gitlab.com/project-emco/core/emco-base/src/ca-certs/pkg/module"
+	"gitlab.com/project-emco/core/emco-base/src/orchestrator/common/emcoerror"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/db"
 )
 
@@ -53,7 +54,10 @@ func (c *CaCertLogicalCloudClient) CreateLogicalCloud(logicalCloud CaCertLogical
 
 	if lcExists &&
 		failIfExists {
-		return CaCertLogicalCloud{}, lcExists, errors.New("LogicalCloud already exists")
+		return CaCertLogicalCloud{}, lcExists, &emcoerror.Error{
+			Message: module.CaCertLogicalCloudAlreadyExists,
+			Reason:  emcoerror.Conflict,
+		}
 	}
 
 	if err := db.DBconn.Insert(c.dbInfo.StoreName, key, nil, c.dbInfo.TagMeta, logicalCloud); err != nil {
@@ -109,7 +113,10 @@ func (c *CaCertLogicalCloudClient) GetLogicalCloud(logicalCloud, cert, project s
 	}
 
 	if len(value) == 0 {
-		return CaCertLogicalCloud{}, errors.New("LogicalCloud not found")
+		return CaCertLogicalCloud{}, &emcoerror.Error{
+			Message: module.CaCertLogicalCloudNotFound,
+			Reason:  emcoerror.NotFound,
+		}
 	}
 
 	if value != nil {
@@ -120,5 +127,8 @@ func (c *CaCertLogicalCloudClient) GetLogicalCloud(logicalCloud, cert, project s
 		return lc, nil
 	}
 
-	return CaCertLogicalCloud{}, errors.New("Unknown Error")
+	return CaCertLogicalCloud{}, &emcoerror.Error{
+		Message: emcoerror.UnknownErrorMessage,
+		Reason:  emcoerror.Unknown,
+	}
 }

src/ca-certs/pkg/module/cert.go
@@ -4,9 +4,10 @@
 package module
 
 import (
+	"fmt"
 	"reflect"
 
-	"github.com/pkg/errors"
+	"gitlab.com/project-emco/core/emco-base/src/orchestrator/common/emcoerror"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/appcontext"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/db"
 	"gitlab.com/project-emco/core/emco-base/src/orchestrator/pkg/infra/logutils"
@@ -48,7 +49,10 @@ func (c *CaCertClient) CreateCert(cert CaCert, failIfExists bool) (CaCert, bool,
 
 	if certExists &&
 		failIfExists {
-		return CaCert{}, certExists, errors.New("Certificate already exists")
+		return CaCert{}, certExists, &emcoerror.Error{
+			Message: CaCertAlreadyExists,
+			Reason:  emcoerror.Conflict,
+		}
 	}
 
 	if err := db.DBconn.Insert(c.dbInfo.StoreName, c.dbKey, nil, c.dbInfo.TagMeta, cert); err != nil {
@@ -90,7 +94,10 @@ func (c *CaCertClient) GetCert() (CaCert, error) {
 	}
 
 	if len(value) == 0 {
-		return CaCert{}, errors.New("Certificate not found")
+		return CaCert{}, &emcoerror.Error{
+			Message: CaCertNotFound,
+			Reason:  emcoerror.NotFound,
+		}
 	}
 
 	if value != nil {
@@ -101,7 +108,10 @@ func (c *CaCertClient) GetCert() (CaCert, error) {
 		return cert, nil
 	}
 
-	return CaCert{}, errors.New("Unknown Error")
+	return CaCert{}, &emcoerror.Error{
+		Message: emcoerror.UnknownErrorMessage,
+		Reason:  emcoerror.Unknown,
+	}
 }
 
 // UpdateCert update the caCert
@@ -124,7 +134,10 @@ func (c *CaCertClient) VerifyStateBeforeDelete(cert, lifecycle string) error {
 
 	if cState == state.StateEnum.Instantiated ||
 		cState == state.StateEnum.InstantiateStopped {
-		err := errors.Errorf("%s must be terminated for CaCert %s before it can be deleted", lifecycle, cert)
+		err := &emcoerror.Error{
+			Message: fmt.Sprintf("%s must be terminated for CaCert %s before it can be deleted", lifecycle, cert),
+			Reason:  emcoerror.Conflict,
+		}
 		logutils.Error("",
 			logutils.Fields{
 				"Error": err.Error()})
@@ -139,7 +152,10 @@ func (c *CaCertClient) VerifyStateBeforeDelete(cert, lifecycle string) error {
 		if err == nil &&
 			!(acStatus.Status == appcontext.AppContextStatusEnum.Terminated ||
 				acStatus.Status == appcontext.AppContextStatusEnum.TerminateFailed) {
-			err := errors.Errorf("%s termination has not completed for CaCert %s", lifecycle, cert)
+			err := &emcoerror.Error{
+				Message: fmt.Sprintf("%s termination has not completed for CaCert %s", lifecycle, cert),
+				Reason:  emcoerror.Conflict,
+			}
 			logutils.Error("",
 				logutils.Fields{
 					"Error": err.Error()})
@@ -182,7 +198,10 @@ func (c *CaCertClient) VerifyStateBeforeUpdate(cert, lifecycle string) error {
 	}
 
 	if cState != state.StateEnum.Created {
-		err := errors.Errorf("Failed to update the CaCert. %s for the CaCert %s is in %s state", lifecycle, cert, cState)
+		err := &emcoerror.Error{
+			Message: fmt.Sprintf("Failed to update the CaCert. %s for the CaCert %s is in %s state", lifecycle, cert, cState),
+			Reason:  emcoerror.Conflict,
+		}
 		logutils.Error("",
 			logutils.Fields{
 				"Error": err.Error()})